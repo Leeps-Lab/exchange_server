cc5f3345bf2454d3f48d1953a633c0eeed305147
David Malec <dlmalec@gmail.com>
2016-10-15 23:42:06 -0400

    First pass at OUCH protocol

---
+++ OuchServer/ouch_messages.py
@@ -0,0 +1,141 @@
+# https://nasdaqtrader.com/content/technicalsupport/specifications/TradingProducts/OUCH4.2.pdf
+from protocol_message_primitives import *
+
+class OuchFields(ProtocolFieldEnum):
+    msg_type = ('c', 'Identifies the type of this message')
+    order_token = ('14s', 'todo')
+    buy_sell_indicator = ('c', 'todo')
+    shares = ('I', 'todo')
+    stock = ('8s', 'todo')
+    price = ('I', 'todo')
+    time_in_force = ('I', 'todo')
+    firm = ('4s', 'todo')
+    display = ('c', 'todo')
+    capacity= ('c', 'todo')
+    intermarket_sweep_eligibility = ('c', 'todo')
+    minimum_quantity = ('I', 'todo')
+    cross_type = ('c', 'todo')
+    replacement_order_token = ('14s', 'todo')
+    # client message only
+    customer_type = ('c', 'todo')
+    existing_order_token = ('14s', 'todo')
+    # server messages only
+    timestamp = ('Q', 'todo')
+    event_code = ('c', 'todo')
+    order_reference_number = ('Q', 'todo')
+    order_state = ('c', 'todo')
+    bbo_weight_indicator = ('c', 'todo')
+    previous_order_token = ('14s', 'todo')
+    decrement_shares = ('I', 'todo')
+    reason = ('c', 'todo')
+    quantity_prevented_from_trading = ('I', 'todo')
+    execution_price = ('I', 'todo')
+    liquidity_flag = ('c', 'todo')
+    executed_shares = ('I', 'todo')
+    match_number = ('Q', 'todo')
+    reference_price = ('I', 'todo')
+    reference_price_type = ('c', 'todo')
+    
+class OuchHeader(NamedFieldSequence):
+    __slots__ = ('msg_type',)
+    _protocol_fields = OuchFields
+    
+class OuchPayloadBase(NamedFieldSequence):
+    __slots__ = ()
+    _protocol_fields = OuchFields
+        
+class OuchMessage(ProtocolMessage):
+    _HeaderCls = OuchHeader
+    _PayloadBaseCls = OuchPayloadBase
+    
+class OuchMessageTypeSpec(MessageTypeSpec):
+    _MessageCls = OuchMessage
+ 
+
+LookupByHeaderBytesMixin = create_attr_lookup_mixin(
+    'LookupByHeaderBytesMixin_ClientMsgs', 'header_bytes')    
+class OuchClientMessages(LookupByHeaderBytesMixin, OuchMessageTypeSpec, DuplicateFreeEnum):
+    EnterOrder = ({'msg_type': b'O'}, [ 
+            'order_token', 'buy_sell_indicator', 'shares', 'stock', 
+            'price', 'time_in_force', 'firm', 'display', 'capacity', 
+            'intermarket_sweep_eligibility', 'minimum_quantity', 
+            'cross_type', 'customer_type'
+        ])
+    ReplaceOrder = ({'msg_type': b'U'}, [
+            'existing_order_token', 'replacement_order_token',
+            'shares', 'price', 'time_in_force', 'display',
+            'intermarket_sweep_eligibility', 'minimum_quantity'
+        ])
+    CancelOrder = ({'msg_type': b'X'}, [
+            'order_token', 'shares'
+        ])
+    ModifyOrder = ({'msg_type': b'M'}, [ 
+            'order_token', 'buy_sell_indicator', 'shares'
+        ])
+    TradeNow = ({'msg_type': b'N'}, [
+            'order_token'
+        ])
+
+LookupByHeaderBytesMixin = create_attr_lookup_mixin(
+    'LookupByHeaderBytesMixin_ServerMsgs', 'header_bytes')    
+class OuchServerMessages(LookupByHeaderBytesMixin, OuchMessageTypeSpec, DuplicateFreeEnum):
+    SystemEvent = ({'msg_type': b'S'}, [
+            'timestamp', 'event_code'
+        ])
+    Accepted = ({'msg_type': b'A'}, [
+            'timestamp', 'order_token', 'buy_sell_indicator', 'shares',
+            'stock', 'price', 'time_in_force', 'firm', 'display',
+            'order_reference_number', 'capacity', 
+            'intermarket_sweep_eligibility', 'minimum_quantity',
+            'cross_type', 'order_state', 'bbo_weight_indicator'])
+    Replaced = ({'msg_type': b'U'}, [
+            'timestamp', 'replacement_order_token', 
+            'buy_sell_indicator', 'shares', 'stock', 'price', 
+            'time_in_force', 'firm', 'display', 'order_reference_number',
+            'capacity', 'intermarket_sweep_eligibility', 
+            'minimum_quantity', 'cross_type', 'order_state',
+            'previous_order_token', 'bbo_weight_indicator'
+        ])    
+    Canceled = ({'msg_type': b'C'}, [
+            'timestamp', 'order_token', 'decrement_shares', 'reason'
+        ])
+    AIQCanceled = ({'msg_type': b'D'}, [
+            'timestamp', 'order_token', 'decrement_shares', 'reason',
+            'quantity_prevented_from_trading', 'execution_price',
+            'liquidity_flag'
+        ])
+    Executed = ({'msg_type': b'E'}, [
+            'timestamp', 'order_token', 'executed_shares',
+            'execution_price', 'liquidity_flag', 'match_number'
+        ])
+    BrokenTrade = ({'msg_type': b'B'}, [
+            'timestamp', 'order_token', 'match_number', 'reason'
+        ])
+    ExecutedWithReferencePrice = ({'msg_type': b'G'}, [
+            'timestamp', 'order_token', 'executed_shares',
+            'execution_price', 'liquidity_flag', 'match_number',
+            'reference_price', 'reference_price_type'
+        ])
+    TradeCorrection = ({'msg_type': b'F'}, [
+            'timestamp', 'order_token', 'executed_shares',
+            'execution_price', 'liquidity_flag', 'match_number', 
+            'reason'
+        ])
+    Rejected = ({'msg_type': b'J'}, [
+            'timestamp', 'order_token', 'reason'
+        ])
+    CancelPending = ({'msg_type': b'P'}, [
+            'timestamp', 'order_token'
+        ])
+    CancelReject = ({'msg_type': b'I'}, [
+            'timestamp', 'order_token'
+        ])
+    OrderPriorityUpdate = ({'msg_type': b'T'}, [
+            'timestamp', 'order_token', 'price', 'display', 
+            'order_reference_number'
+        ])
+    OrderModified = ({'msg_type': b'M'}, [
+            'timestamp', 'order_token', 'buy_sell_indicator', 'shares'])
+    TradeNow = ({'msg_type': b'N'}, [
+            'timestamp', 'order_token'
+        ])    
---
+++ OuchServer/protocol_message_primitives.py
@@ -0,0 +1,201 @@
+import enum
+import struct
+
+class DuplicateFreeEnum(enum.Enum):
+    # force unique values, as per docs:
+    # https://docs.python.org/3/library/enum.html#duplicatefreeenum
+    def __init__(self, *args, **kwargs):
+        cls = self.__class__
+        for element in cls:
+            if element.value == self.value:
+                raise ValueError(
+                    'aliases not allowed in DuplicateFreeEnum: {} --> {}'
+                    .format(element.name, self.name))
+
+class ProtocolField(object):
+    def __init__(self, type_spec, description):
+        self._type_spec = type_spec
+        self._description = description
+
+    @property
+    def type_spec(self):
+        return self._type_spec
+
+    @property
+    def description(self):
+        return self._description
+
+                
+class ProtocolFieldEnum(ProtocolField, DuplicateFreeEnum):
+    pass
+
+class NamedFieldSequenceSerializerMeta(type):
+    def __init__(cls, name, bases, namespace):
+        cls._struct_formatter = struct.Struct(
+            cls._wire_format + 
+            ''.join(cls._protocol_fields[field].type_spec
+                    for field in cls.__slots__))
+        super().__init__(name, bases, namespace)
+        
+    @property
+    def size(cls):
+        return cls._struct_formatter.size
+    
+class NamedFieldSequence(object, metaclass=NamedFieldSequenceSerializerMeta):
+    _struct_formatter = None
+    _protocol_fields = None
+    _wire_format = '!'  # network byte order and no padding
+    __slots__ = ()
+    
+    @classmethod
+    def from_bytes(cls, source_bytes):
+        return cls(*cls._struct_formatter.unpack(source_bytes))
+
+    def to_bytes(self):
+        return bytes(self)
+    
+    def __init__(self, *args, **kwargs):
+        if args and kwargs:
+            raise ValueError('NamedFieldSequence can only take one of positional or keyword arguments')
+        elif len(args) == len(self.__slots__):
+            for (slot, value) in zip(self.__slots__, args):
+                setattr(self, slot, value)
+        elif args: 
+            raise ValueError('%s has %d slots, got %d values' 
+                             % (self.__class__, len(self.__slots__), len(args)))
+            
+        else:  # kwargs
+            for slot in self.__slots__:
+                setattr(self, slot, kwargs.get(slot, None))
+
+    def __bytes__(self):
+        assert not any(getattr(self, slot) is None for slot in self.__slots__)            
+        return self._struct_formatter.pack(
+            *(getattr(self, slot) for slot in self.__slots__))
+    
+    def __len__(self):
+        return len(self.__slots__)
+    def __iter__(self):
+        yield from iter(self.__slots__)
+    def iteritems(self):
+        yield from ((s, getattr(self, s)) for s in self.__slots__)
+    def __getitem__(self, key):
+        if key in self.__slots__:
+            return getattr(self, key)
+        else:
+            raise KeyError('key %s not found' % (key))
+    def __setitem__(self, key, value):
+        if key in self.__slots__:
+            setattr(self, key, value)
+        else:
+            raise KeyError('key %s not found' % (key))
+    def __delitem__(self, key):
+        if key in self.__slots__:
+            raise TypeError('%s has immutable keys' % (self.__class__.__name__))
+        else:
+            raise KeyError('key %s not found' % (key))
+            
+    def __contains__(self, key):
+        return key in self.__slots__
+    
+
+class ProtocolMessage(object):
+    _HeaderCls = None
+    _PayloadBaseCls = None
+    
+    def __init__(self, message_type_spec, *args, **kwargs):
+        self._message_type_spec = message_type_spec
+        self.payload = message_type_spec.PayloadCls(*args, **kwargs)
+            
+    def to_bytes(self, header=False):
+        return bytes(self) if header else bytes(self.payload)
+            
+    @classmethod
+    def from_payload_bytes(cls, message_type_spec, payload_bytes):
+        message = cls(message_type_spec)
+        message.payload = message_type_spec.PayloadCls.from_bytes(payload_bytes)        
+        return message
+        
+    def __bytes__(self):
+        return bytes(self._message_type_spec._header) + bytes(self.payload)
+        
+    @property
+    def header(self):
+        return self._message_type_spec.header
+    def __len__(self):
+        return len(self.payload)
+    def __iter__(self):
+        yield from iter(self.payload)
+    def iteritems(self):
+        yield from self.payload.iteritems()
+    def __getitem__(self, key):
+        return self.payload[key]
+    def __setitem__(self, key, value):
+        self.payload[key] = value
+    def __delitem__(self, key):
+        del self.payload[key]            
+    def __contains__(self, key):
+        return key in self.payload
+    
+class MessageTypeSpec(object):
+    _MessageCls = None
+    
+    def __init__(self, header_field_values, payload_fields, name=None):
+        if name is None:
+            name = self.name
+        cls = self.__class__
+        self._header = cls._MessageCls._HeaderCls(**header_field_values)
+        self._PayloadCls = type(
+            name, (cls._MessageCls._PayloadBaseCls,), 
+            {'__slots__': payload_fields})
+    
+    def __call__(self, *args, **kwargs):
+        return self.MessageCls(self, *args, **kwargs)
+
+    def from_bytes(self, message_bytes, header=False):
+        if header:
+            header_len = self._MessageCls._HeaderCls.size
+            header_bytes = message_bytes[:header_len]
+            message_bytes = message_bytes[header_len:]
+            if header_bytes != self.header_bytes:
+                raise ValueError('header mismatch!')
+        return self.MessageCls.from_payload_bytes(self, message_bytes)
+    
+    @property
+    def MessageCls(self):
+        return self._MessageCls
+    @property
+    def header(self):
+        return self._header
+    @property
+    def header_bytes(self):
+        return bytes(self._header)
+    @property
+    def PayloadCls(self):
+        return self._PayloadCls
+    @property
+    def payload_size(self):
+        return self._PayloadCls.size
+        
+def create_attr_lookup_mixin(cls_name, attr_name):
+    lookup_table_name = '_{}_lookup_table'.format(attr_name)    
+
+    class LookupByAttrMixin(object):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            cls = self.__class__
+            lookup_table = getattr(cls, lookup_table_name)
+            attr_value = getattr(self, attr_name)
+            if attr_value in lookup_table:
+                raise ValueError('{}: attribute {} has duplicate value {}'
+                                 .format(self.__class__.__name__, attr_name, attr_value))
+            else:
+                lookup_table[attr_value] = self    
+    
+    def lookup(cls, attr_value):
+        lookup_table = getattr(cls, lookup_table_name)
+        return lookup_table[attr_value]
+    
+    return type(cls_name, (LookupByAttrMixin,),
+               {('lookup_by_' + attr_name): classmethod(lookup),
+                 lookup_table_name: dict()})
