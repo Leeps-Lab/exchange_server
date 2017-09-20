098fde5fdd6424455ff541a7809523331b6c208a
root <root@ip-172-31-43-27.us-west-2.compute.internal>
2017-09-20 17:38:52 +0000

    changed fba script

--- run_fba_groups.sh
+++ run_fba_groups.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+./stop_all.sh
 groups=$1
+flag=$2
 timestamp=$(date +'%Y-%m-%d_%H:%M:%S')
 
 if [ -z "$1" ];
@@ -11,5 +13,5 @@ fi
 for i in `seq $groups`;
 do
 	websockify 800$i localhost:1200$i &
-	python3 run_exchange_server.py --host localhost --port 1200$i --mechanism fba --interval 3  --book_log FBA_DATA/${timestamp}_group_$i.log &
+	python3 run_exchange_server.py --host localhost --port 1200$i --mechanism fba --interval 3  --book_log FBA_DATA/${timestamp}_group_$i.log --${flag} &
 done
