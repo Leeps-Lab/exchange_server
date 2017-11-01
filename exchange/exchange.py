2d46e64cadba6c194a33ae076e0af77a861c5f13
root <root@ip-172-31-26-9.us-west-1.compute.internal>
2017-11-01 01:55:27 +0000

    tests for syncing start times

--- exchange/exchange.py
+++ exchange/exchange.py
@@ -59,11 +59,13 @@ class Exchange:
         else:
             self.order_store.clear_order_store()
             self.order_book.reset_book()
-            self.start_time = nanoseconds_since_midnight()
-            log.info(printTime(self.start_time))
-	    #log.info(system_event_message['timestamp'])
-            #log.info(printTime(system_event_message['timestamp']))
-            #log.info(int.from_bytes(system_event_message['leeps_timestamp'], byteorder='little'))
+            #self.start_time = nanoseconds_since_midnight()
+            #log.info(printTime(self.start_time))
+            self.start_time = system_event_message['timestamp']
+            log.info(system_event_message['timestamp'])
+            log.info(type(system_event_message['timestamp']))
+            log.info(printTime(system_event_message['timestamp']))
+            #log.info(int.from_bytes(system_event_message['timestamp'], byteorder='little'))
     def accepted_from_enter(self, enter_order_message, timestamp, order_reference_number, order_state=b'L', bbo_weight_indicator=b' '):
         m=OuchServerMessages.Accepted(
             timestamp=timestamp,
