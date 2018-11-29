1e775308b405167d86566e9af6780165a54963e3
hademircii <hdemirci@ucsc.edu>
2018-11-29 08:33:15 -0800

    adding missing fields to UTP message

--- UTPServer/utp_messages.py
+++ UTPServer/utp_messages.py
@@ -6,11 +6,22 @@ class UTPFields(ProtocolFieldEnum):
     msg_type = ('c', 'message type')
     orig = ('c', 'market center originator id')
     submarket_id = ('c', 'sub market center id')
-    sip_time = ('l', 'sip timestamp')
-    timestamp_1 = ('l', 'participant timestamp')
-    part_token = ('l', 'participant token')
-    timestamp_2 = ('l', 'FINRA timestamp')
+    sip_time = ('Q', 'sip timestamp')
+    timestamp_1 = ('Q', 'participant timestamp')
+    part_token = ('Q', 'participant token')
+    timestamp_2 = ('Q', 'FINRA timestamp')
     symbol = ('11s', 'symbol')
+    bid_price = ('Q', 'bid price')
+    bid_size= ('I', 'bid size')
+    ask_price = ('Q', 'ask price')
+    ask_size = ('I', 'ask size')
+    quote_condition = ('c', 'quote condition')
+    sip_gen_update = ('c', 'sip generated update flag')
+    luld_bbo_indicator = ('c', 'luld bbo indicator')
+    rri = ('c', 'retail interest indicator')
+    nbbo_indicator = ('c', 'nbbo appendage indicator')
+    luld_nbbo_indicator = ('c', 'luld nbbo indicator')
+    finra_adf_indicator = ('c', 'finra adf mpid appendage indicator')
 
 class UTPHeader(NamedFieldSequence):
     __slots__ = ('version', 'msg_category', 'msg_type')
@@ -39,13 +50,17 @@ LookupByHeaderBytesMixin = create_attr_lookup_mixin(
     'LookupByHeaderBytesMixin_ClientMsgs', 'header_bytes')
 
 
-class UTPClientMessages(LookupByHeaderBytesMixin, UTPMessageTypeSpec,
+class UTPMessages(LookupByHeaderBytesMixin, UTPMessageTypeSpec,
                         DuplicateFreeEnum):
     LongFormQuoteMessage = ('{orig}:{submarket_id}:{timestamp_2}:{symbol}',
                     {'version': b'1', 
                     'msg_category': b'Q',
                     'msg_type': b'F'},
                     ['timestamp_2', 'symbol', 'orig', 'submarket_id',
-                    'sip_time', 'timestamp_1', 'part_token'])
+                    'sip_time', 'timestamp_1', 'part_token', 'bid_price', 
+                    'bid_size', 'ask_price', 'ask_size', 'quote_condition',
+                    'sip_gen_update', 'luld_bbo_indicator', 'rri', 'nbbo_indicator',
+                    'luld_nbbo_indicator', 'finra_adf_indicator' ]
+            )
 
 
--- utp_tests.py
+++ utp_tests.py
@@ -1,15 +1,27 @@
 from UTPServer import utp_messages, utp_feed
 import time
+from random import randrange
 
 def test_message():
     msg = utp_messages.UTPClientMessages.LongFormQuoteMessage(
-        orig = b'D',   # FINRA
-        submarket_id = b'L', 
-        sip_time = int(time.time()),
-        timestamp_1 = int(time.time()),
-        part_token = 95060000,
-        timestamp_2 = int(time.time()),
-        symbol = b'LEEPS',
+        orig=b'D',   # FINRA
+        submarket_id=b' ', 
+        sip_time=int(time.time()),
+        timestamp_1=int(time.time()),
+        part_token=95060000,
+        timestamp_2=int(time.time()),
+        symbol=b'LEEPS',
+        bid_price=randrange(50,60),
+        bid_size=1,
+        ask_price=randrange(50,60),
+        ask_size=1,
+        quote_condition=b'R',
+        sip_gen_update=b' ',
+        luld_bbo_indicator=b' ',
+        rri=b' ',
+        nbbo_indicator=b' ',
+        luld_nbbo_indicator=b' ',
+        finra_adf_indicator=b' '
     )
     bytes_msg= bytes(msg)
     print('bytes message:{} with length {}.'.format(bytes_msg, len(bytes_msg)))
