f371cfc1563d5e047264d1d72629d3edddc23605
hademircii <hdemirci@ucsc.edu>
2018-12-01 13:22:04 -0800

    an iterable message feed

--- UTPServer/utp_feed.py
+++ UTPServer/utp_feed.py
@@ -1,10 +1,13 @@
 import csv
-from collections import deque
-
+from UTPServer.utp_messages import UTPMessages
+from OuchServer.ouch_server import nanoseconds_since_midnight
+import logging as log
 
 class FeedUnit:
     desc = ''
     __slots__ = ()
+    protocol_cls = None
+    protocol_defaults = {}
 
     def __init__(self, **kwargs):
         for slot in self.__slots__:
@@ -17,26 +20,60 @@ class FeedUnit:
                 kwargs[field] = row.pop(0)
                 if not row:
                     break
+        kwargs = cls.field_check(**kwargs)
         return cls(**kwargs)
+
+    @classmethod   
+    def field_check(cls, **kwargs):
+        pass
   
     def dictize(self):
         return {slot: getattr(self, slot) for slot in self.__slots__}
 
-    def post_read(self):
-        pass
-        
-
-class BCSFeedUnit(FeedUnit):
-    desc = 'time, fundamental value (V) pairs' 
-    __slots__ = ('time', 'fundamental', 'nbo', 'nbb')
-
-    def post_read(self, d_up=50, d_down=50):
-        V = self.fundamental = int(self.fundamental)
-        self.nbo = V + d_up
-        self.nbb = V - d_down
-    
+    def __bytes__(self, **defaults):
+        bytes_message = {**self.protocol_defaults}
+        for slot in self.__slots__:
+            if slot in self.protocol_defaults:
+                bytes_message[slot] = getattr(self, slot)
+        return bytes(self.protocol_cls(**bytes_message))
+   
     def __str__(self):
         return str(self.dictize())
+
+class BCSFeedUnit(FeedUnit):
+    desc = 'time, best bid and offer in a row.' 
+    __slots__ = ('time', 'bid_price', 'ask_price')
+    protocol_cls = UTPMessages.LongFormQuoteMessage
+    protocol_defaults = {
+        'orig':b'D',   # FINRA
+        'submarket_id':b'X',
+        'msg_type': b'X', 
+        'sip_time':nanoseconds_since_midnight(),
+        'timestamp_1':nanoseconds_since_midnight(),
+        'part_token':95060000,
+        'timestamp_2':nanoseconds_since_midnight(),
+        'symbol':b'LEEPS',
+        'bid_price':1001000,
+        'bid_size':1,
+        'ask_price':1002000,
+        'ask_size':1,
+        'quote_condition':b'R',
+        'sip_gen_update':b'X',
+        'luld_bbo_indicator':b'X',
+        'rri':b'X',
+        'nbbo_indicator':b'X',
+        'luld_nbbo_indicator':b'X',
+        'finra_adf_indicator':b'X'
+    }
+
+    @classmethod    
+    def field_check(cls, **kwargs):
+        edited_kwargs = {}
+        for k, v in kwargs.items():
+            if not isinstance(v, int):
+                int_v = int(v)
+            edited_kwargs[k] = int_v
+        return edited_kwargs
 
 class Feed:
 
@@ -52,14 +89,20 @@ class Feed:
                 raw_feed = raw_feed[1:]
             for row in raw_feed:
                 line = self.unit_cls.from_list(row)
-                line.post_read()
                 self.feed.append(line)
 
     def __iter__(self):
-        return next(self)
-
-    def __next__(self):
-        yield from reversed(self.feed)
+        line = None
+        for row in self.feed:
+            try:
+                line = bytes(row)
+            except Exception as e:
+                log.exception('error encoding row, ignoring: %s', e)
+            yield line
+        
+    def __str__(self):
+        return '\n'.join(str(feed_unit) for feed_unit in self.feed)
+    
 
 
     
--- utp_tests.py
+++ utp_tests.py
@@ -1,37 +1,11 @@
-from UTPServer import utp_messages, utp_feed
+from UTPServer.utp_feed import *
 import time
 from random import randrange
 
-def test_message():
-    msg = utp_messages.UTPClientMessages.LongFormQuoteMessage(
-        orig=b'D',   # FINRA
-        submarket_id=b' ', 
-        sip_time=int(time.time()),
-        timestamp_1=int(time.time()),
-        part_token=95060000,
-        timestamp_2=int(time.time()),
-        symbol=b'LEEPS',
-        bid_price=randrange(50,60),
-        bid_size=1,
-        ask_price=randrange(50,60),
-        ask_size=1,
-        quote_condition=b'R',
-        sip_gen_update=b' ',
-        luld_bbo_indicator=b' ',
-        rri=b' ',
-        nbbo_indicator=b' ',
-        luld_nbbo_indicator=b' ',
-        finra_adf_indicator=b' '
-    )
-    bytes_msg= bytes(msg)
-    print('bytes message:{} with length {}.'.format(bytes_msg, len(bytes_msg)))
-
-def test_feed():
-    feed = utp_feed.Feed(utp_feed.BCSFeedUnit)
-    feed.from_csv('UTPServer/test_data/test.csv')
-    for line in feed:
-        print(line)
 
 if __name__ == '__main__':
-    test_message()
-    test_feed()
+    feed = Feed(BCSFeedUnit)
+    feed.from_csv('UTPServer/test_data/test.csv')
+    print(feed)
+    for row in feed:
+        print(row)
